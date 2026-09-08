889f1e400b181ca59e7784544434fa096581d559
firmanagam <agamgans89@gmail.com>
2026-09-08 18:40:34 +0700

    chore(migrations): fix stray tab in ModelMigrations ContentReport entry

--- services/api/app/db/migrations/migration.go
+++ services/api/app/db/migrations/migration.go
@@ -103,6 +103,6 @@ var ModelMigrations []interface{} = []interface{}{
 	model.LessonStep{},
 	model.UserLessonProgress{},
 	model.AdzanSound{},
-	model.	ContentReport{},
+	model.ContentReport{},
 	model.ContentAuditLog{},
 }
